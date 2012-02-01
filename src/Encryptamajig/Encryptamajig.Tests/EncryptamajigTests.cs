c47aa782f9396488800591b5ccbeb415a5e7ca3c
John Bubriski <bubriski@gmail.com>
2012-02-01 12:07:04 -0500

    Implemented better crytography practices while simplifying the API and usage. Fixed the unit tests accordingly and added a Console app tester. Allowed Nuget to rebuild itself on build.

--- src/Encryptamajig/Encryptamajig.Tests/EncryptamajigTests.cs
+++ src/Encryptamajig/Encryptamajig.Tests/EncryptamajigTests.cs
@@ -8,20 +8,23 @@
     [TestFixture]
     public class EncryptamajigTests
     {
-        private readonly string _plainText = "Some text to encrypt";
-        private readonly string _key = "Some key to use for encrypting data";
-        private readonly string _userName1 = "UserName 1";
-        private readonly string _userName2 = "UserName 2";
+        // A test credit card number
+        private static readonly string _plainText = "4111111111111111";
+
+        // The key should be a random string of characters that an attacker could never guess
+        private static readonly string _key = "Something you can't guess";
 
         [Test]
-        public void AesManaged_EncryptionRoundtripWithRandomInputs_ReturnsOriginalText()
+        public void AesManaged_EncryptionRoundtripWithRandomKey_ReturnsOriginalText()
         {
             // Arrange
             var aesManaged = new AesManaged();
 
+            var key = Convert.ToBase64String(aesManaged.Key);
+
             // Act
-            var encrypted = AesEncryptamajig.Encrypt(_plainText, aesManaged.Key, aesManaged.IV);
-            var roundtrip = AesEncryptamajig.Decrypt(encrypted, aesManaged.Key, aesManaged.IV);
+            var encrypted = AesEncryptamajig.Encrypt(_plainText, key);
+            var roundtrip = AesEncryptamajig.Decrypt(encrypted, key);
 
             Debug.WriteLine(_plainText);
             Debug.WriteLine(encrypted);
@@ -36,12 +39,9 @@
         public void AesManaged_EncryptionRoundtripWithCustomInputs_ReturnsOriginalText()
         {
             // Arrange
-            var key = AesEncryptamajig.GetKey(_key);
-            var iv = AesEncryptamajig.GetIv(_userName1);
-
             // Act
-            var encrypted = AesEncryptamajig.Encrypt(_plainText, key, iv);
-            var roundtrip = AesEncryptamajig.Decrypt(encrypted, key, iv);
+            var encrypted = AesEncryptamajig.Encrypt(_plainText, _key);
+            var roundtrip = AesEncryptamajig.Decrypt(encrypted, _key);
 
             Debug.WriteLine(_plainText);
             Debug.WriteLine(encrypted);
@@ -53,26 +53,37 @@
         }
 
         [Test]
-        public void AesManaged_ReencryptionWithSameKeyAndIv_ReturnsSameText()
+        public void AesManaged_ReencryptionWithSameKey_ReturnsSameText()
         {
             // Arrange
             var aesManaged = new AesManaged();
 
-            // Act
-            var encrypted = AesEncryptamajig.Encrypt(_plainText, aesManaged.Key, aesManaged.IV);
-            var roundtrip = AesEncryptamajig.Decrypt(encrypted, aesManaged.Key, aesManaged.IV);
+            var key = Convert.ToBase64String(aesManaged.Key);
 
-            Debug.WriteLine(_plainText);
-            Debug.WriteLine(encrypted);
-            Debug.WriteLine(roundtrip);
+            // Act
+            var encrypted1 = AesEncryptamajig.Encrypt(_plainText, key);
+            var roundtrip1 = AesEncryptamajig.Decrypt(encrypted1, key);
+
+            Debug.WriteLine("Plain Text: " + _plainText);
+            Debug.WriteLine("Encrypted: " + encrypted1);
+            Debug.WriteLine("Roundtrip: " + roundtrip1);
 
             // Act 2 (encrypt the same data again)
-            encrypted = AesEncryptamajig.Encrypt(_plainText, aesManaged.Key, aesManaged.IV);
-            roundtrip = AesEncryptamajig.Decrypt(encrypted, aesManaged.Key, aesManaged.IV);
+            var encrypted2 = AesEncryptamajig.Encrypt(_plainText, key);
+            var roundtrip2 = AesEncryptamajig.Decrypt(encrypted2, key);
+
+
+            Debug.WriteLine("Encrypted again: " + encrypted2);
+            Debug.WriteLine("Roundtrip again: " + roundtrip2);
 
             // Assert
-            Assert.AreNotEqual(_plainText, encrypted);
-            Assert.AreEqual(_plainText, roundtrip);
+            Assert.AreNotEqual(_plainText, encrypted1);
+            Assert.AreNotEqual(_plainText, encrypted2);
+
+            Assert.AreEqual(_plainText, roundtrip1);
+            Assert.AreEqual(_plainText, roundtrip2);
+
+            Assert.AreNotEqual(encrypted1, encrypted2);
         }
 
         [Test]
@@ -82,155 +93,22 @@
             // Arrange
             var aesManaged = new AesManaged();
 
-            var originalKey = aesManaged.Key;
-            var originalIv = aesManaged.IV;
-
-            // Act
-            var encrypted = AesEncryptamajig.Encrypt(_plainText, originalKey, originalIv);
+            var originalKey = Convert.ToBase64String(aesManaged.Key);
 
             // This should generate a new key
             aesManaged = new AesManaged();
+            var newKey = Convert.ToBase64String(aesManaged.Key);
 
-            Debug.WriteLine("Org Key = " + Convert.ToBase64String(originalKey));
-            Debug.WriteLine("New Key = " + Convert.ToBase64String(aesManaged.Key));
+            // Act
+            var encrypted = AesEncryptamajig.Encrypt(_plainText, originalKey);
 
-            string roundtrip = AesEncryptamajig.Decrypt(encrypted, aesManaged.Key, originalIv);
+            Debug.WriteLine("Org Key = " + originalKey);
+            Debug.WriteLine("New Key = " + newKey);
+
+            string roundtrip = AesEncryptamajig.Decrypt(encrypted, newKey);
 
             // Assert
             throw new Exception("This should have thrown a CryptographicException!");
-        }
-
-        [Test]
-        public void Decrypt_WithDifferentIv_ReturnsDifferentResult()
-        {
-            // Arrange
-            var aesManaged = new AesManaged();
-
-            var originalKey = aesManaged.Key;
-            var originalIv = aesManaged.IV;
-
-            // Act
-            var encrypted = AesEncryptamajig.Encrypt(_plainText, originalKey, originalIv);
-
-            // This should generate a new key
-            aesManaged = new AesManaged();
-
-            var roundtrip = AesEncryptamajig.Decrypt(encrypted, originalKey, aesManaged.IV);
-
-            // Assert
-            Debug.WriteLine("Org IV: " + Convert.ToBase64String(originalIv));
-            Debug.WriteLine("New IV: " + Convert.ToBase64String(aesManaged.IV));
-
-            Debug.WriteLine(_plainText);
-            Debug.WriteLine("Encrypted: " + encrypted);
-            Debug.WriteLine("ROundtrup: " + roundtrip);
-
-            Assert.AreNotEqual(_plainText, roundtrip);
-        }
-
-        [Test]
-        public void GetKey_GetIvToo_KeyAndIvAreDifferent()
-        {
-            var key = AesEncryptamajig.GetKey(_key);
-            var iv = AesEncryptamajig.GetIv(_userName1);
-
-            Console.WriteLine(Convert.ToBase64String(key));
-            Console.WriteLine(Convert.ToBase64String(iv));
-
-            Assert.AreNotEqual(key, iv);
-        }
-
-        [Test]
-        public void GetKey_CalledTwice_KeyIsTheSame()
-        {
-            var keybytes = AesEncryptamajig.GetKey(_key);
-            var keyBytesAgain = AesEncryptamajig.GetKey(_key);
-
-            Console.WriteLine(Convert.ToBase64String(keybytes));
-            Console.WriteLine(Convert.ToBase64String(keyBytesAgain));
-
-            Assert.AreEqual(keybytes, keyBytesAgain);
-        }
-
-        [Test]
-        public void GetIv_CalledTwice_IvIsTheSame()
-        {
-            var ivbytes = AesEncryptamajig.GetIv(_userName1);
-            var ivbytesAgain = AesEncryptamajig.GetIv(_userName1);
-
-            Console.WriteLine(Convert.ToBase64String(ivbytes));
-            Console.WriteLine(Convert.ToBase64String(ivbytesAgain));
-
-            Assert.AreEqual(ivbytes, ivbytesAgain);
-        }
-
-        [Test]
-        public void GetKey_CalledWithPlainTextKey_GeneratesValidKeySize()
-        {
-            // Arrange
-            var aesManaged = new AesManaged();
-
-            // Act
-            var key = AesEncryptamajig.GetKey(_key);
-
-            // Assert
-            Assert.That(aesManaged.ValidKeySize(key.Length * 8));
-        }
-
-        [Test]
-        public void GetIv_CalledWIthUserName_GeneratesValidIvSize()
-        {
-            // Arrange
-            var aesManaged = new AesManaged();
-
-            // Act
-            var iv = AesEncryptamajig.GetIv(_userName1);
-
-            // Assert
-            Assert.That(aesManaged.BlockSize == (iv.Length * 8));
-        }
-
-        [Test]
-        public void GetIv_SameInputs_GeneratesSameIvs()
-        {
-            // Arrange
-            // Act
-            var iv1 = AesEncryptamajig.GetIv(_userName1);
-            var iv2 = AesEncryptamajig.GetIv(_userName1);
-
-            // Assert
-            Assert.AreEqual(iv1, iv2);
-        }
-
-        [Test]
-        public void GetIv_DifferentInputs_GeneratesDifferentIvs()
-        {
-            // Arrange
-            // Act
-            var iv1 = AesEncryptamajig.GetIv(_userName1);
-            var iv2 = AesEncryptamajig.GetIv(_userName2);
-
-            // Assert
-            Assert.AreNotEqual(iv1, iv2);
-        }
-
-        [Test]
-        public void AesManaged_NewInstances_ReturnsNewKeyAndIv()
-        {
-            // Arrange
-            // Act
-            var aesManaged = new AesManaged();
-            var aesManaged2 = new AesManaged();
-
-            Debug.WriteLine(Convert.ToBase64String(aesManaged.Key));
-            Debug.WriteLine(Convert.ToBase64String(aesManaged2.Key));
-
-            Debug.WriteLine(Convert.ToBase64String(aesManaged.IV));
-            Debug.WriteLine(Convert.ToBase64String(aesManaged2.IV));
-
-            // Assert
-            Assert.AreNotEqual(aesManaged.Key, aesManaged2.Key);
-            Assert.AreNotEqual(aesManaged.IV, aesManaged2.IV);
         }
     }
 }
--- src/Encryptamajig/Encryptamajig/AesEncryptamajig.cs
+++ src/Encryptamajig/Encryptamajig/AesEncryptamajig.cs
@@ -2,135 +2,100 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Security.Cryptography;
 
     /// <summary>
     /// A simple wrapper to the AesManaged class and the AES algorithm.
-    /// To create a new Key and IV simple "new up" an AesManaged object and grab the Key and IV from that.
-    /// Make sure to save the Key and IV if you want to decrypt your data later!
+    /// Requires a securely stored key which should be a random string of characters that an attacker could never guess.
+    /// Make sure to save the Key if you want to decrypt your data later!
+    /// If you're using this with a Web app, put the key in the web.config and encrypt the web.config.
     /// </summary>
     public class AesEncryptamajig
     {
-        // Remove the static salt and incorporate an easy method for managing it.
-        private static readonly byte[] _salt = new byte[] { 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
+        // 16 bytes = 128 bits
+        private static readonly int _saltSize = 16;
 
         /// <summary>
-        /// Encrypts the plainText input using the given Key and IV.
-        /// You can generate Keys and IV's from the GetKey() and GetIv() methods.
+        /// Encrypts the plainText input using the given Key.
+        /// A 128 bit random salt will be generated and prepended to the ciphertext before it is base64 encoded.
         /// </summary>
-        /// <param name="plainText"></param>
-        /// <param name="key"></param>
-        /// <param name="iv"></param>
-        /// <returns></returns>
-        public static string Encrypt(string plainText, string key, string iv, string salt)
+        /// <param name="plainText">The plain text to encrypt.</param>
+        /// <param name="key">The plain text encryption key.</param>
+        /// <returns>The salt and the ciphertext, Base64 encoded for convenience.</returns>
+        public static string Encrypt(string plainText, string key)
         {
             if (string.IsNullOrEmpty(plainText))
                 throw new ArgumentNullException("plainText");
             if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException("key");
-            if (string.IsNullOrEmpty(iv))
-                throw new ArgumentNullException("iv");
-            if (string.IsNullOrEmpty(salt))
-                throw new ArgumentNullException("salt");
 
-            // TODO: Convert the salt to a byte array
-            var saltBytes = new byte[0];
-
-            using (var aesManaged = new AesManaged())
-            using (var deriveBytes = new Rfc2898DeriveBytes(key, saltBytes))
+            // Derive a new Salt and IV from the Key
+            using (var keyDerivationFunction = new Rfc2898DeriveBytes(key, _saltSize))
             {
-                // Derive the Key and the IV using Rfc2898DeriveBytes to make sure we get the same values given the same inputs
-                var keyBytes = deriveBytes.GetBytes(aesManaged.KeySize);
-                var ivBytes = deriveBytes.GetBytes(aesManaged.BlockSize);
+                var saltBytes = keyDerivationFunction.Salt;
+                var keyBytes = keyDerivationFunction.GetBytes(32);
+                var ivBytes = keyDerivationFunction.GetBytes(16);
 
                 // Create an encryptor to perform the stream transform.
-                var encryptor = aesManaged.CreateEncryptor(keyBytes, ivBytes);
-
                 // Create the streams used for encryption.
+                using (var aesManaged = new AesManaged())
+                using (var encryptor = aesManaged.CreateEncryptor(keyBytes, ivBytes))
                 using (var memoryStream = new MemoryStream())
-                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                using (var streamWriter = new StreamWriter(cryptoStream))
                 {
-                    // Send the data through the StreamWriter, through the CryptoStream, to the underlying MemoryStream
-                    streamWriter.Write(plainText);
+                    using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    using (var streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        // Send the data through the StreamWriter, through the CryptoStream, to the underlying MemoryStream
+                        streamWriter.Write(plainText);
+                    }
 
                     // Return the encrypted bytes from the memory stream, in Base64 form so we can send it right to a database (if we want).
-                    return Convert.ToBase64String(memoryStream.ToArray());
+                    var cipherTextBytes = memoryStream.ToArray();
+                    Array.Resize(ref saltBytes, saltBytes.Length + cipherTextBytes.Length);
+                    Array.Copy(cipherTextBytes, 0, saltBytes, _saltSize, cipherTextBytes.Length);
+
+                    return Convert.ToBase64String(saltBytes);
                 }
             }
         }
 
         /// <summary>
-        /// Decrypts the cipherText using the Key and the IV.
+        /// Decrypts the ciphertext using the Key.
         /// </summary>
-        /// <param name="cipherText"></param>
-        /// <param name="key"></param>
-        /// <param name="iv"></param>
-        /// <returns></returns>
-        public static string Decrypt(string cipherText, string key, string iv, string salt)
+        /// <param name="ciphertext">The ciphertext to decrypt.</param>
+        /// <param name="key">The plain text encryption key.</param>
+        /// <returns>The decrypted text.</returns>
+        public static string Decrypt(string ciphertext, string key)
         {
-            if (string.IsNullOrEmpty(cipherText))
+            if (string.IsNullOrEmpty(ciphertext))
                 throw new ArgumentNullException("cipherText");
             if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException("key");
-            if (string.IsNullOrEmpty(iv))
-                throw new ArgumentNullException("iv");
-            if (string.IsNullOrEmpty(key))
-                throw new ArgumentNullException("salt");
 
-            // TODO: Convert the salt to a byte array
-            var saltBytes = new byte[0];
+            // Extract the salt from our ciphertext
+            var allTheBytes = Convert.FromBase64String(ciphertext);
+            var saltBytes = allTheBytes.Take(_saltSize).ToArray();
+            var ciphertextBytes = allTheBytes.Skip(_saltSize).Take(allTheBytes.Length - _saltSize).ToArray();
 
-            using (var aesManaged = new AesManaged())
-            using (var deriveBytes = new Rfc2898DeriveBytes(key, saltBytes))
+            using (var keyDerivationFunction = new Rfc2898DeriveBytes(key, saltBytes))
             {
-                // Derive the Key and the IV using Rfc2898DeriveBytes to make sure we get the same values given the same inputs
-                var keyBytes = deriveBytes.GetBytes(aesManaged.KeySize);
-                var ivBytes = deriveBytes.GetBytes(aesManaged.BlockSize);
+                // Derive the previous IV from the Key and Salt
+                var keyBytes = keyDerivationFunction.GetBytes(32);
+                var ivBytes = keyDerivationFunction.GetBytes(16);
 
                 // Create a decrytor to perform the stream transform.
-                var decryptor = aesManaged.CreateDecryptor(keyBytes, ivBytes);
-
                 // Create the streams used for decryption.
-                using (var memoryStream = new MemoryStream(Convert.FromBase64String(cipherText)))
+                using (var aesManaged = new AesManaged())
+                using (var decryptor = aesManaged.CreateDecryptor(keyBytes, ivBytes))
+                using (var memoryStream = new MemoryStream(ciphertextBytes))
                 using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
                 using (var streamReader = new StreamReader(cryptoStream))
                 {
-                    // Read the decrypted bytes from the decrypting stream
-                    // and place them in a string.
+                    // Return the decrypted bytes from the decrypting stream.
                     return streamReader.ReadToEnd();
                 }
             }
         }
-
-        /// <summary>
-        /// This will convert the plain text key to a byte array for use with the Encrypt/Decrypt methods.
-        /// Make sure you are storing the plain text key in a secure location.
-        /// For example, if you store the encryption key in the web.config, make sure you encrypt the web.config.
-        /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        private static byte[] GetKey(string key)
-        {
-            var deriveBytes = new Rfc2898DeriveBytes(key, _salt);
-
-            return deriveBytes.GetBytes(32);
-        }
-
-        /// <summary>
-        /// This will generate an IV (Intialization Vector) based on the input text. The same input text will generate the same IV.
-        /// The same IV is required to decrypt the data, so make sure the input text is being saved with the encrypted data.
-        /// Don't use the same IV input text for all your data.
-        /// For example, if you're encrypting user records, use this method to generate the IV from the user's username.
-        /// </summary>
-        /// <param name="text"></param>
-        /// <returns></returns>
-        private static byte[] GetIv(string text)
-        {
-            var deriveBytes = new Rfc2898DeriveBytes(text, _salt);
-
-            return deriveBytes.GetBytes(16);
-        }
-
     }
 }
